4dd1cbca5aff37f44184001006a5b38376d5090c
Matheus Gomes Leitao <matheus.g.leitao@aluno.senai.br>
2025-05-30 14:49:26 -0300

    Exiba os 4 primeiros artigos em uma lista

--- scripts/exercicio14.js
+++ scripts/exercicio14.js
@@ -12,5 +12,3 @@ let artigosBlog = [
 let arquivosrecentes = artigosBlog.slice(0,4)
 console.log(`A nova lista ficou: ${arquivosrecentes}`);
     
-aç
-Fa um relatório somente tendo a primeira semana de férias dos colabradores
