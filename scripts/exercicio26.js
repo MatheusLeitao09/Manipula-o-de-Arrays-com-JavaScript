2d3455a3c77a97e72ea77c18ad4f632f680fe453
Matheus Gomes Leitao <matheus.g.leitao@aluno.senai.br>
2025-05-30 16:09:43 -0300

    Inverta a lista de histórico

---
+++ scripts/exercicio26.js
@@ -0,0 +1,12 @@
+let historico = [
+  "login",
+  "painel",
+  "relatórios",
+  "detalhes do cliente",
+  "editar dados",
+  "configurações",
+  "logout"
+];
+
+historico.reverse();
+console.log(`A lista de histórico invertida ficou: ${historico}`);
