d1f30fae39a76fe2654f143e11595015ebc0a09a
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-05-30 11:29:12 -0300

    Retire a ultima pessoa da lista

---
+++ scripts/exercicio5.js
@@ -0,0 +1,11 @@
+let importados = [
+  "carregador portátil",
+  "cabo HDMI",
+  "leitor biométrico",
+  "controle sem fio",
+  "webcam 4K"
+
+]
+
+importados.shift();
+console.log(`A lista com o primeiro elemento removido ficou: ${importados}`);
---
+++ scripts/exercicios4.js
@@ -0,0 +1,6 @@
+let inscritos = [
+  "Pedro", "Larissa", "João", "Amanda", "Lucas", "Mariana"
+];
+
+inscritos.pop();
+console.log(`A lista sem a última inscrição ficou: ${inscritos}`);
