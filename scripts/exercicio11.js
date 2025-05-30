1d6ac9e827d2bb0c0a6c9fa040a7cdf99738a9e6
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-05-30 11:53:47 -0300

    Deixe a lista aparecendo em uma lista só

---
+++ scripts/exercicio11.js
@@ -0,0 +1,10 @@
+let ingredientes = [
+  "farinha de trigo",
+  "fermento",
+  "leite",
+  "ovos",
+  "açúcar",
+  "manteiga"
+];
+
+console.log(`A lista em uma linha só ficou: ${ingredientes.join(', ')}`);
