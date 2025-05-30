b664f882ee099e5e9e3d54acf30d339adfdc1456
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-05-30 11:44:22 -0300

    Adiciona duas pessoas no inicio

---
+++ scripts/exercicio7.js
@@ -0,0 +1,10 @@
+let filaAtendimento = [
+  "cliente 003",
+  "cliente 004",
+  "cliente 005",
+  "cliente 006",
+  "cliente 007"
+];
+ filaAtendimento.unshift('cliente 001', 'cliente 002')
+ console.log(`A lista final com os clientes vips adicionados ficou: ${filaAtendimento}`);
+ 
