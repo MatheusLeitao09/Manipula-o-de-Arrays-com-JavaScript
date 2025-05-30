38cb50e5f1a5dee7c666d8eefc928e8c32de1380
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-05-30 11:52:00 -0300

    Verifique se a lista possui um elemento

---
+++ scripts/exercicio10.js
@@ -0,0 +1,9 @@
+let estoque = [
+  "mesa para PC",
+  "cadeira gamer",
+  "monitor 27 polegadas",
+  "hub USB",
+  "mouse sem fio"
+];
+
+console.log(`A lista possui cadeira gamer? ${estoque.includes("cadeira gamer")}`);
