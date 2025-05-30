b65091cc5c1bfa29bc03ec008b0cba15beacfc70
Matheus Gomes Leitao <matheus.g.leitao@aluno.senai.br>
2025-05-30 15:59:17 -0300

    Verificar os alunos com nota 7 ou maior

---
+++ scripts/exercicio22.js
@@ -0,0 +1,11 @@
+let alunos = [
+  { nome: "Carlos", media: 6.5 },
+  { nome: "Fernanda", media: 7.2 },
+  { nome: "Lucas", media: 8.1 },
+  { nome: "Marina", media: 5.9 },
+  { nome: "João", media: 7.0 },
+  { nome: "Aline", media: 9.3 }
+];
+
+let aprovados = alunos.filter(nota => nota.media >= 7);
+console.log(aprovados);
