8e682eb47f253e8e062c183526543d497b9bf02a
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-05-30 11:25:23 -0300

    Retire o ultimo elemento da lista

---
+++ scripts/exercicio3.js
@@ -0,0 +1,12 @@
+let producao = [
+  "mousepad",
+  "placa de vídeo",
+  "fonte de alimentação",
+  "HD externo",
+  "notebook gamer",
+  "roteador"
+];
+
+ producao.pop();
+ console.log(`A lista com o último elemento retirado ficou: ${producao}` );
+ 
