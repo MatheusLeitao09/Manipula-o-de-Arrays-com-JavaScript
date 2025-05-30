7cd83261659afc8886982f4fb7717d08825a2cdc
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-05-30 11:47:59 -0300

    Adiciona duas meninas no começo da lista

---
+++ scripts/exercicio8.js
@@ -0,0 +1,10 @@
+let chamada = [
+  "Carlos",
+  "Fernanda",
+  "João",
+  "Mariana",
+  "Lucas",
+  "Camila"
+];
+chamada.unshift('Beatriz', 'Rafaela')
+console.log(`A lista com as duas meninas adicionadas no começo da lista ficou: ${chamada}`);
