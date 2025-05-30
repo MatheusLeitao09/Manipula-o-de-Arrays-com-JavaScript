bdefb4a68fd5c38343fa20b873dad4910aa8e41a
Matheus Gomes Leitao <matheus.g.leitao@aluno.senai.br>
2025-05-30 16:08:04 -0300

    Inverta a ordem da lista

---
+++ scripts/exercicio25.js
@@ -0,0 +1,13 @@
+let atualizacoes = [
+  "Backup diário concluído",
+  "Servidor reiniciado",
+  "Nova versão implantada",
+  "Logs exportados",
+  "Firewall atualizado",
+  "Monitoramento ativado",
+  "Banco de dados otimizado",
+  "Script de segurança executado"
+];
+
+atualizacoes.reverse();
+console.log(`A lista invertida ficou: ${atualizacoes}`);
