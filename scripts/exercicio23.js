a1f433a9b440a620295498533a7f473520258718
Matheus Gomes Leitao <matheus.g.leitao@aluno.senai.br>
2025-05-30 16:01:01 -0300

    Organiza em ordem alfabetica os nomes dos candidatos

---
+++ scripts/exercicio23.js
@@ -0,0 +1,13 @@
+let candidatos = [
+  "Marcos",
+  "Aline",
+  "Carla",
+  "João",
+  "Fernanda",
+  "Lucas",
+  "Bruno",
+  "Isabela"
+];
+
+candidatos.sort();
+console.log(`A lista dos candidatos em ordem alfabética ficou: ${candidatos}`);
