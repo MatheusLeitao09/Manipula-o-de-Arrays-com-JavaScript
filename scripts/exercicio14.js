81cad60f7194560491750c7966b2da696c2748ca
Matheus Gomes Leitao <matheus.g.leitao@aluno.senai.br>
2025-05-30 14:47:52 -0300

    Fa um relatório somente tendo a primeira semana de férias dos colabradores

--- scripts/exercicio14.js
+++ scripts/exercicio14.js
@@ -12,3 +12,5 @@ let artigosBlog = [
 let arquivosrecentes = artigosBlog.slice(0,4)
 console.log(`A nova lista ficou: ${arquivosrecentes}`);
     
+aç
+Fa um relatório somente tendo a primeira semana de férias dos colabradores
