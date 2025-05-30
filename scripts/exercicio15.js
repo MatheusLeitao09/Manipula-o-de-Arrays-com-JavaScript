eabcd807c7f458767bebc177d0df0902eac60b09
Matheus Gomes Leitao <matheus.g.leitao@aluno.senai.br>
2025-05-30 14:55:35 -0300

    Remove os dois itens do menu de navegação na posicao 2

---
+++ scripts/exercicio15.js
@@ -0,0 +1,12 @@
+let menuNavegacao = [
+  "Início",
+  "Serviços",
+  "Portfólio",
+  "Blog",
+  "Contato",
+  "Login",
+  "Área do cliente"
+];
+
+menuNavegacao.splice(2,2)
+console.log(`A lista final após a remoção ficou: ${menuNavegacao}`);
