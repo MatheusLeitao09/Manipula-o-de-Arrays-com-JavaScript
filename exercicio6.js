1b16e3ef8ba85a4ca68dd74411a85289cba0e704
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-05-30 11:35:41 -0300

    Remove a primeira mensagem da lista

---
+++ exercicio6.js
@@ -0,0 +1,10 @@
+let mensagens = [
+  "Propaganda indevida",
+  "Bom dia, posso ajudar?",
+  "Preciso de suporte técnico",
+  "Claro, qual o problema?",
+  "Erro ao acessar conta"
+];
+
+mensagens.shift();
+console.log(`Com a primeira mensagem removida, a lista ficou assim: ${mensagens}`);
