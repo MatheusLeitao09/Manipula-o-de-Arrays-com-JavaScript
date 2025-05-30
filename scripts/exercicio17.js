ee6a7e46a6a0b8f491eab8c31a251a67734bb7f9
Matheus Gomes Leitao <matheus.g.leitao@aluno.senai.br>
2025-05-30 15:21:15 -0300

    Junte a lista A com a lista B, para formar uma só

---
+++ scripts/exercicio17.js
@@ -0,0 +1,16 @@
+let usuariosSistemaA = [
+  "aline.souza",
+  "maria.ferreira",
+  "carlos.lima",
+  "ana.martins"
+];
+
+let usuariosSistemaB = [
+  "joao.silva",
+  "paulo.pereira",
+  "fernanda.alves",
+  "marcelo.cardoso"
+]
+
+let listaFInal = usuariosSistemaA.concat(usuariosSistemaB)
+console.log(`A lista com todos os usuários ficou: ${listaFInal}`);
