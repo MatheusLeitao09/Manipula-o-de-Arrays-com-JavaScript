8651b8f9787150264d41ee9019c6b2b56b1aeb97
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-05-30 11:50:23 -0300

    Verifique se a lista possui um elemento

---
+++ scripts/exercicio9.js
@@ -0,0 +1,8 @@
+let presenca = [
+  "João Lima",
+  "Carla Nunes",
+  "Amanda Souza",
+  "Bruno Ribeiro",
+  "Fernanda Sales"
+];
+console.log(`A lista possui a funcionária Amanda Souza? ${presenca.includes("Amanda Souza")}`);
