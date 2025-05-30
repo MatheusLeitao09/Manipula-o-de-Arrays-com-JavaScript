a0bba520110c567af2544933481cd3ef7fc4f118
Matheus Gomes Leitao <matheus.g.leitao@aluno.senai.br>
2025-05-30 15:24:18 -0300

    Junte o harware com os perifericos para formar uma lista só

---
+++ scripts/exercicio18.js
@@ -0,0 +1,17 @@
+let hardware = [
+  "processador i7",
+  "placa-mãe",
+  "memória RAM 16GB",
+  "SSD 1TB"
+];
+
+let perifericos = [
+  "teclado gamer",
+  "mouse ótico",
+  "webcam HD",
+  "fone com microfone"
+];
+
+let listafinal = hardware.concat(perifericos)
+console.log(`A lista final com os dois juntos ficou: ${listafinal}`);
+
