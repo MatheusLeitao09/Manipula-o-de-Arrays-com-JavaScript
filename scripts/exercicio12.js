a7a39a505e63855991635e7bf6e547ad89f33c44
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2025-05-30 11:56:23 -0300

    Separa a lista com o ( - )

---
+++ scripts/exercicio12.js
@@ -0,0 +1,9 @@
+let palavrasChave = [
+  "desenvolvimento",
+  "frontend",
+  "javascript",
+  "css",
+  "react"
+];
+
+console.log(`A lista com os itens separados ficou: ${palavrasChave.join(' - ')}`);
