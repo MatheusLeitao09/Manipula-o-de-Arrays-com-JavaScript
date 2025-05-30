7656216f6713929bd258d729ce1481f94d610111
Matheus Gomes Leitao <matheus.g.leitao@aluno.senai.br>
2025-05-30 14:59:50 -0300

    Substitua as disciplinas na lista

---
+++ scripts/exercicio16.js
@@ -0,0 +1,13 @@
+let gradeCurricular = [
+  "Matemática",
+  "Português",
+  "Biologia",
+  "Física",
+  "História",
+  "Educação Física",
+  "Inglês"
+];
+
+gradeCurricular.splice(2, 2, "Biologia", "Física")
+console.log(`A grade curricular ficou: ${gradeCurricular}`);
+    
