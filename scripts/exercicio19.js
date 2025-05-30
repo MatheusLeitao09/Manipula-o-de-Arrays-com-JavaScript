0d3e45a751208b87e1c34d39149a53dad76b02c6
Matheus Gomes Leitao <matheus.g.leitao@aluno.senai.br>
2025-05-30 15:30:39 -0300

    Deixe todo o codigo em letra maiusculas

---
+++ scripts/exercicio19.js
@@ -0,0 +1,13 @@
+let codigos = [
+  "abc123",
+  "def456",
+  "ghi789",
+  "jkl012",
+  "mno345",
+  "pqr678",
+  "stu901",
+  "vwx234"
+];
+
+let letraMaiuscula = codigos.map(codigo => codigo.toUpperCase());
+console.log(`Os códigos com todas as letras maiúsculas ficaram: ${letraMaiuscula}`);
